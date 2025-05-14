96247327dc5f4d36e49f851be67534c3357bc4cc
ChandraShekhar <csyadav0513@gmail.com>
2025-05-14 09:24:20 +0530

    Implement CRUD functionality for Tweet model with views for listing, creating, editing, and deleting tweets

--- tweet/views.py
+++ tweet/views.py
@@ -1,11 +1,67 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Tweet
+from .forms import TweetForm
+from django.shortcuts import get_object_or_404 , redirect
+
 
 # Create your views here.
 
 def index(request):
-    return render(request , "index.html")
+    return render(request, "index.html")
 
 
 def hello(request):
     return HttpResponse("Hello Babu Boi 😆")
+
+#! Tweet List 🚵
+def tweet_list(request):
+    tweets = Tweet.objects.all().order("-created_at")
+    return render(request, "tweet_list.html", {"tweets": tweets})
+
+# Tweet Create 🍨
+def tweet_create(request):
+    if request.method == "POST":
+      form = TweetForm(request.POST , request.FILES)
+      if form.is_valid():
+        tweet = form.save(commit=False)
+        tweet.user = request.user
+        tweet.save()
+        return redirect("tweet_list")
+          
+          
+      pass
+    else:
+        form = TweetForm()
+
+    return render(request, "tweet_form.html", {"form": form})
+    
+
+#! Tweet Edit ✏️
+def tweet_edit(request , tweet_id):
+   tweet = get_object_or_404(Tweet, pk=tweet_id , user = request.user )
+
+   if request.method == "POST":
+      form = TweetForm(request.POST, request.FILES , instance=tweet)
+
+      if form.is_valid():
+        tweet = form.save(commit=False)
+        tweet.user = request.user
+        tweet.save()
+        return redirect("tweet_list")
+      
+     
+   else:
+      form = TweetForm(instance=tweet)
+
+   return render(request, "tweet_form.html", {"form": form})
+
+#! Tweet Delete 🙃
+def tweet_delete(request , tweet_id):
+   tweet = get_object_or_404(Tweet , pk=tweet_id , user = request.user)
+
+   if request.method == "POST":
+      tweet.delete()
+      return redirect("tweet_list")
+   
+   return render(request, "tweet_confrim_delete.html", {"tweet": tweet})
